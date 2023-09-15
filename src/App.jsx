df1a89525b138bdeefc78d0216852f855c0900ac
Zahid Hasan <67813593+zhzahid1911509@users.noreply.github.com>
2023-09-15 14:58:34 +0600

    toastify added for duplicate course adding

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import './App.css'
 import Cart from './components/Cart/Cart'
 import Courses from './components/Courses/Courses'
+//import { toastError } from './components/Toast/toast';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
 
 
 function App() {
@@ -9,21 +13,43 @@ function App() {
   const [cart, setCart] = useState([]);
 
   const handleAddCourse = course =>{
-    console.log(course.title);
-    const newCart = [...cart, course];
+    //console.log(course.title);
+    const isExist = cart.find(courseCart => courseCart.id == course.id);
+    if(isExist){
+      toast.error("This course has already been added");
+    }
+    else{
+      const newCart = [...cart, course];
     setCart(newCart);
+    }
   }
 
   return (
     <>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
        <h1 className='text-3xl font-bold text-center my-4'>Course Registration</h1>
+      
       <div className='flex flex-col md:flex-row mx-5'>
        
        <Courses handleAddCourse={handleAddCourse}></Courses>
        
-       <Cart cart={cart}></Cart>       
+       <Cart cart={cart}></Cart>  
+
+      
 
       </div>
+      
     </>
     
   )
